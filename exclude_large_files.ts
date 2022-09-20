5adb1bc79a5c27ad4bc13a92c1af9cc784557479
Zequez <zequez@gmail.com>
2022-09-20 03:55:17 -0300

    Auto saved by Logseq
    Former-commit-id: 5fd875f779297dde7e24a23479ef78ccc57f67a7

--- exclude_large_files.ts
+++ exclude_large_files.ts
@@ -1,10 +1,18 @@
 console.log("Excluding large files from Git repo");
-const p = Deno.run({
-  cmd: "find ./assets -type f -size +5MB".split(" "),
-  stdout: "piped",
-  stderr: "piped",
-});
 
-const files = new TextDecoder().decode(await p.output()).split(/\n/);
+function filesLargerThan5MB() {
+  const p = Deno.run({
+    cmd: "find ./assets -type f -size +5MB".split(" "),
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+  const files = new TextDecoder()
+    .decode(await p.output())
+    .split(/\n/)
+    .filter((v) => !!v);
+
+  return files;
+}
 
 console.log(files);
